4ad8200901f18988262b97475a3c26b4342adca6
sudhanshu201 <113603064+sudhanshu201@users.noreply.github.com>
2023-03-26 17:58:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-const url = "https://jsonplaceholder.typicode.com/posts/1";
+
 
 //your JS code here. If required.
+const apiUrl = "https://jsonplaceholder.typicode.com/todos/1";
+
+async function fetchData(callback) {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    callback(data.title);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+document.getElementById("btn").addEventListener("click", async function() {
+  fetchData(function(title) {
+    document.getElementById("output").innerHTML = title;
+  });
+});
+
