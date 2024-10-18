75aaf7cb73e2460fb35e22602f19d830b7b70822
sudhanshu201 <113603064+sudhanshu201@users.noreply.github.com>
2024-10-18 06:40:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-
-
 //your JS code here. If required.
 const apiUrl = "https://jsonplaceholder.typicode.com/todos/1";
 
-async function fetchData(callback) {
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    callback(data.title);
-  } catch (error) {
-    console.log(error);
-  }
+document.getElementById("btn").addEventListener("click", fetching)
+
+async function fetching(){
+	const fetchedData = await fetch(apiUrl)
+	const data = await fetchedData.json()
+    //console.log(data)
+	
+	document.getElementById("output").innerHTML=data.title
 }
 
-document.getElementById("btn").addEventListener("click", async function() {
-  fetchData(function(title) {
-    document.getElementById("output").innerHTML = title;
-  });
-});
 
